027e7aaf5cd7fcd9d738a24d5962e44e2b027fc8
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-02-13 17:02:07 +0100

    added maplab_msgs dep

--- bin/graph_monitor.py
+++ bin/graph_monitor.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Header
+from maplab_msgs import Graph, Trajectory, TrajectoryNode
 
 
 class GraphMonitor(object):
@@ -10,13 +11,19 @@ class GraphMonitor(object):
         rospy.loginfo("[GraphMonitor] Initializing...")
 
         graph_topic = rospy.get_param("~graph_topic")
+        traj_topic = rospy.get_param("~traj_topic")
+
         rospy.Subscriber(graph_topic, Header, self.graph_callback)
+        rospy.Subscriber(graph_topic, Header, self.traj_callback)
 
         rospy.loginfo("[GraphMonitor] Graph monitor is set up.")
 
 
     def graph_callback(self, msg):
         rospy.loginfo("[GraphMonitor] Received graph message.")
+
+    def traj_callback(self, msg):
+        rospy.loginfo("[GraphMonitor] Received trajectory message.")
 
 
 

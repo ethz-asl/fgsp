1ad749be824755f676d51a100b73271d7647dd50
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-07-02 09:26:48 +0200

    wip path graph building

--- bin/global_graph.py
+++ bin/global_graph.py
@@ -4,7 +4,7 @@ import numpy as np
 from pygsp import graphs, filters, reduction
 from geometry_msgs.msg import Point
 from maplab_msgs.msg import Graph
-
+from scipy import spatial
 
 class GlobalGraph(object):
     def __init__(self, reduced=False):
@@ -33,21 +33,21 @@ class GlobalGraph(object):
 
     def build(self, graph_msg):
         self.coords = self.read_coordinates(graph_msg)
-        rospy.logdebug("[Graph] Building with coords " + str(self.coords.shape))
+        rospy.logdebug("[GlobalGraph] Building with coords " + str(self.coords.shape))
         self.adj = self.read_adjacency(graph_msg)
-        rospy.logdebug("[Graph] Building with adj: " + str(self.adj.shape))
+        rospy.logdebug("[GlobalGraph] Building with adj: " + str(self.adj.shape))
         self.submap_ind = self.read_submap_indices(graph_msg)
-        rospy.logdebug("[Graph] Building with ind: " + str(len(self.submap_ind)))
+        rospy.logdebug("[GlobalGraph] Building with ind: " + str(len(self.submap_ind)))
 
         if len(self.adj.tolist()) == 0:
-            rospy.loginfo(f"[Graph] Adjacency matrix is empty. Aborting graph building.")
+            rospy.loginfo(f"[GlobalGraph] Adjacency matrix is empty. Aborting graph building.")
             return
         self.G = graphs.Graph(self.adj)
         if self.G.N != self.coords.shape[0]:
-            rospy.logerr(f"[Graph] Graph size is {self.G.N} but coords are {self.coords.shape}")
+            rospy.logerr(f"[GlobalGraph] Graph size is {self.G.N} but coords are {self.coords.shape}")
             return
         if self.G.N <= 1:
-            rospy.logdebug("[Graph] Graph vertex count is less than 2.")
+            rospy.logdebug("[GlobalGraph] Graph vertex count is less than 2.")
             return
 
         self.G.set_coordinates(self.coords[:,[0,1]])
@@ -58,8 +58,16 @@ class GlobalGraph(object):
 
         self.graph_seq = graph_msg.header.seq
         self.is_built = True
-        rospy.loginfo("[Graph] Building complete")
+        rospy.loginfo("[GlobalGraph] Building complete")
         self.latest_graph_msg = graph_msg
+
+    def build_from_path(self, path_msg):
+        n_poses = len(path_msg.poses)
+        if n_poses <= 0:
+            rospy.logerr(f"[GlobalGraph] Received empty path message.")
+            return False
+        self.coords = read_coordinates_from_poses(path_msg.poses)
+        rospy.loginfo("[GlobalGraph] Building with coords " + str(self.coords.shape))
 
     def read_coordinates(self, graph_msg):
         n_coords = len(graph_msg.coords)
@@ -71,12 +79,41 @@ class GlobalGraph(object):
 
         return coords
 
+    def read_coordinates_from_poses(self, poses):
+        n_coords = len(poses)
+        coords = np.zeros((n_coords, 3))
+        for i in range(0, n_coords):
+            coords[i,0] = poses[i].pose.position.x
+            coords[i,1] = poses[i].pose.position.y
+            coords[i,2] = poses[i].pose.position.z
+        return coords
+
     def read_adjacency(self, graph_msg):
         n_coords = len(graph_msg.coords)
         adj = np.zeros((n_coords, n_coords))
         for i in range(n_coords):
             for j in range(n_coords):
                 adj[i,j] = graph_msg.adjacency_matrix[j + i * n_coords]
+
+        return adj
+
+    def create_adjacency_from_poses(self, coords):
+        n_coords = coords.shape[0]
+        adj = np.zeros((n_coords, n_coords))
+        tree = spatial.KDTree(coords)
+        max_pos_dist = 5
+        n_nearest_neighbors = 5
+        sigma = 1
+        normlization = 2*(sigma**2)
+        for i in range(n_coords):
+            nn_dists, nn_indices = tree.query(coords[i,:], p = 2, k = n_nearest_neighbors)
+            nn_indices = [nn_indices] if n_nearest_neighbors == 1 else nn_indices
+            for nn_i in range(nn_indices):
+                if nn_i == i:
+                    continue
+                dist = spatial.distance.euclidean(coords[nn_i,:], coords[i,:])
+                if dist <= max_pos_dist:
+                    adj[i,nn_i] = np.exp(dist/normalization)
 
         return adj
 

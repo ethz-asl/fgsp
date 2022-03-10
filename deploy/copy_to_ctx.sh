8b639612321d63dad7e98d7c03d15493d4874a33
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-03-10 15:03:35 +0100

    wip container creation

--- deploy/copy_to_ctx.sh
+++ deploy/copy_to_ctx.sh
@@ -4,10 +4,21 @@
 script_source=$(readlink -f "$0")
 script_dir=$(dirname "$script_source")
 docker_ctx=${script_dir}/docker_ctx
-bin_dir="${script_dir}/../bin"
+bin_dir=${script_dir}/../bin/
+config_dir=${script_dir}/../config/
 
 # Copy the python files to the context.
-cp "${bin_dir}/*" "${docker_ctx}"
+rsync -a \
+  --exclude '*.pyc' \
+  --exclude '__pycache__' \
+  --exclude '.ipynb_checkpoints' \
+  "${bin_dir}" "${docker_ctx}"
+
+  # Copty the configuration files to the context.
+mkdir -p "${docker_ctx}/config"
+rsync -a \
+  "${config_dir}" "${docker_ctx}/config/"
+
 
 # Copy the installation script to the context.
-cp "${script_dir}/install_simple.sh" "${docker_ctx}"
+# cp "${script_dir}/install_simple.sh" "${docker_ctx}"

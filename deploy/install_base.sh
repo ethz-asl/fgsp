6dc83354aa17cc610e638093e975d7d9e232636e
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-03-11 10:25:13 +0100

    making the images automatically connect to the ROS master

--- deploy/install_base.sh
+++ deploy/install_base.sh
@@ -3,4 +3,4 @@
 # Base install
 apt update
 apt -y upgrade
-apt -y install software-properties-common python2.7 python-pip python-termcolor git curl
+apt -y install software-properties-common python2.7 python-pip python-termcolor git curl iproute2

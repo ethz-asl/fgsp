6d77ab61be46574dce4d370c07ffd45d0482ba9e
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-03-15 12:08:33 +0100

    tested graph monitor

--- deploy/set_env.sh
+++ deploy/set_env.sh
@@ -3,5 +3,5 @@
 . /usr/home/ws/devel/setup.bash
 host_ip=$(ip route | awk '/default/ {print $3}')
 export ROS_MASTER_URI=http://${host_ip}:11311
-export ROS_IP=${host_ip}
+export ROS_HOSTNAME=localhost
 exec "$@"

fixed bug in pos/quat to transformation computation

bin/utils.py
@@ -40,11 +40,13 @@ class Utils(object):
 
     @staticmethod
     def convert_pos_quat_to_transformation(pos, quat):
-        R1 = Rotation.from_quat(quat)
+        # takes xyzw as input
+        R = Rotation.from_quat(np.array([quat[1],quat[2],quat[3],quat[0]]))
         T = np.empty((4, 4))
-        T[0:3, 0:3] = R1.as_dcm()
+        T[0:3, 0:3] = R.as_dcm()
         T[0:3, 3] = pos
         T[3, :] = [0, 0, 0, 1]
+
         return T
 
     @staticmethod
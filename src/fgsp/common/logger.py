b3795df2f4bf6320e1c0ac26b10b90e7bc237188
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-06-08 08:57:28 +0200

    wip logger

---
+++ src/fgsp/common/logger.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+import time
+from font import Font
+
+
+class Logger(object):
+    @staticmethod
+    def LogDebug(msg):
+        ts_ns = time.time() * 1e6
+        print(f'[{ts_ns}] {msg}')
+
+    @staticmethod
+    def LogWarn(msg):
+        ts_ns = time.time() * 1e6
+        print(f'{Font.YELLOW}[{ts_ns}] {msg} {Font.END}')
+
+    @staticmethod
+    def LogERROR(msg):
+        ts_ns = time.time() * 1e6
+        print(f'{Font.RED}[{ts_ns}] {msg} {Font.END}')
+
+    @staticmethod
+    def LogFATAL(msg):
+        ts_ns = time.time() * 1e6
+        print(f'{Font.RED}{FONT.BOLD} [{ts_ns}] {msg} {Font.END}')
+        exit()
+
+
+if __name__ == '__main__':
+    Logger.LogDebug("This is a debug message.")
+    Logger.LogWarn("This is a warning message.")
+    Logger.LogERROR("This is a error message.")

75dbdd371e4f3d76cf8265bcecc3062338505f0b
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-06-09 08:56:31 +0200

    comms to singleton

--- src/fgsp/common/comms.py
+++ src/fgsp/common/comms.py
@@ -3,12 +3,18 @@
 import rclpy
 from rclpy.node import Node
 
-class Comms(Node):
-    def __init__(self):
-        super().__init__('fgsp_comms')
+class Comms(object):
+    _instance = None
+
+    def __new__(self):
+        if self._instance is None:
+            print(f'Creating the object')
+            self._instance = super(Comms, self).__new__(self)
+            self._instance.node = Node('fgsp_comms')
+        return self._instance
 
     def send(self, msg, type, topic):
-        publisher = self.create_publisher(type, topic, 10)
+        publisher = self.node.create_publisher(type, topic, 10)
         publisher.publish(msg)
 
 
@@ -18,11 +24,18 @@ if __name__ == '__main__':
     rclpy.init()
 
     msg = String()
+    msg2 = String()
     msg.data = "Foo"
+    msg2.data = "Bar"
 
     comms = Comms()
-    comms.send(msg, String, "foo")
+    comms.send(msg, String, "/foo")
 
-    rclpy.spin_once(comms, timeout_sec=1)
-    comms.destroy_node()
+    comms2 = Comms()
+    comms2.send(msg2, String, "/foo")
+
+    print(f'Singleton? {comms is comms2}')
+
+    rclpy.spin_once(comms.node, timeout_sec=1)
+    comms.node.destroy_node()
     rclpy.shutdown()

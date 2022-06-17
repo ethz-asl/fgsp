fd5b61a50fd41064525fc885b98e597327c1fef8
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-06-17 11:24:37 +0200

    wip bag write

--- src/fgsp/simulation.py
+++ src/fgsp/simulation.py
@@ -4,10 +4,12 @@ from os.path import exists
 
 import rclpy
 from rclpy.node import Node
-from evo.core.trajectory import PoseTrajectory3D
 from evo.tools import file_interface
 from maplab_msgs.msg import Graph, Trajectory, SubmapConstraint
-from nav_msgs.msg import Path, Odometry
+import numpy as np
+
+from rosbags.serde import serialize_cdr
+from rosbags.rosbag2 import Writer as Rosbag2Writer
 
 from src.fgsp.common.logger import Logger
 
@@ -17,16 +19,18 @@ class Simulation(Node):
         super().__init__('simulation')
         Logger.LogInfo('Simulation: Initializing...')
 
-        self.server_traj_tum = self.get_traj_file('server_file')
-        self.robot_traj_tum = self.get_traj_file('robot_file')
-        if self.server_traj_tum is None or self.robot_traj_tum is None:
+        self.server_traj = self.get_traj_file('server_file')
+        self.robot_traj = self.get_traj_file('robot_file')
+        if self.server_traj is None or self.robot_traj is None:
             Logger.LogError('Simulation: Trajectory files not found!')
             return
 
         print(
-            f'Simulation: Server trajectory: {self.server_traj_tum.get_infos()}')
+            f'Simulation: Server trajectory: {self.server_traj.get_infos()}')
         print(
-            f'Simulation: Robot trajectory: {self.robot_traj_tum.get_infos()}')
+            f'Simulation: Robot trajectory: {self.robot_traj.get_infos()}')
+
+        self.write_odometry(self.robot_traj)
 
     def get_traj_file(self, traj_key):
         print(f'declaring param {traj_key}')
@@ -40,6 +44,50 @@ class Simulation(Node):
         else:
             Logger.LogError(f'Simulation: File does not exist!')
             return None
+
+    def write_odometry(self, robot_traj):
+        bag_file = '/tmp/odometry.bag'
+        ros2_bag_out = Rosbag2Writer(bag_file)
+        ros2_bag_out.open()
+
+        from rosbags.typesys.types import (
+            nav_msgs__msg__Odometry as Odometry,
+            geometry_msgs__msg__PoseWithCovariance as PoseWithCovariance,
+            geometry_msgs__msg__TwistWithCovariance as TwistWithCovariance,
+            geometry_msgs__msg__Twist as Twist,
+            geometry_msgs__msg__Vector3 as Vector3,
+            std_msgs__msg__Header as Header,
+            geometry_msgs__msg__Pose as Pose,
+            geometry_msgs__msg__Point as Position,
+            geometry_msgs__msg__Quaternion as Quaternion,
+            builtin_interfaces__msg__Time as Time)
+
+        msg_type = Odometry.__msgtype__
+        topic = '/odometry'
+        connection = ros2_bag_out.add_connection(topic, msg_type)
+
+        Logger.LogDebug(f'Writing odometry to bag file to: {bag_file}')
+        for stamp, xyz, quat in zip(robot_traj.timestamps, robot_traj.positions_xyz,
+                                    robot_traj.orientations_quat_wxyz):
+            sec = int(stamp // 1)
+            nanosec = int((stamp - sec) * 1e9)
+            time = Time(sec, nanosec)
+            header = Header(time, "map")
+            position = Position(x=xyz[0], y=xyz[1], z=xyz[2])
+            quaternion = Quaternion(w=quat[0], x=quat[1], y=quat[2], z=quat[3])
+            pose = Pose(position, quaternion)
+            pose_with_cov = PoseWithCovariance(pose, np.array([0.0] * 36))
+            vec = Vector3(0.0, 0.0, 0.0)
+            twist = Twist(vec, vec)
+            twist_with_cov = TwistWithCovariance(twist, np.array([0.0] * 36))
+            odom = Odometry(header, "base", pose_with_cov, twist_with_cov)
+
+            serialized_msg = serialize_cdr(odom, msg_type)
+            ros2_bag_out.write(connection, int(stamp * 1e9), serialized_msg)
+        Logger.LogInfo(f'Wrote topic: {topic}')
+        ros2_bag_out.close()
+
+        Logger.LogInfo('Simulation: Writing odometry to bag file done.')
 
 
 def main(args=None):

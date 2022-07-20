0c7027070f8d600db041d81f451632b87b0259cd
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-07-20 10:35:16 +0200

    test

--- src/fgsp/classifier/top_classifier.py
+++ src/fgsp/classifier/top_classifier.py
@@ -7,7 +7,7 @@ class TopClassifier(object):
 
     def __init__(self, top_n=5):
         self.top_n = top_n
-        self.threshold = 0.2
+        self.threshold = 0.15
 
     def classify(self, data):
         n_nodes = data.shape[0]

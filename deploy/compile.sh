40a8a75db75101d36039e7705553b68886f189e9
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-07-27 10:43:22 +0200

    wip compile

--- deploy/compile.sh
+++ deploy/compile.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+cd src/liegroups
+python3 setup.py install --user
+touch COLCON_IGNORE
+
+cd ../pygsp
+python3 setup.py install --user
+touch COLCON_IGNORE
+
+cd ../../
 source /opt/ros/humble/setup.bash
-colcon build
+colcon build --packages-select maplab_msgs
+colcon build --packages-select fgsp

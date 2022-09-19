a531495ab08dfef7d1853bb51691d69e9d8f18c1
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-09-19 09:45:26 +0200

    wip obj pub

--- src/fgsp/tools/object_publisher.py
+++ src/fgsp/tools/object_publisher.py
@@ -13,9 +13,12 @@ from evo.core import sync
 from evo.core import metrics
 from evo.tools import plot
 import matplotlib.pyplot as plt
+from visualization_msgs.msg import Marker, MarkerArray
+from std_msgs.msg import ColorRGBA
 
 
 from src.fgsp.common.logger import Logger
+from src.fgsp.common.utils import Utils
 
 
 class ObjectPublisher(Node):
@@ -24,12 +27,61 @@ class ObjectPublisher(Node):
         Logger.Verbosity = 5
         Logger.LogInfo('ObjectPublisher: Initializing...')
 
-        color = self.get_param('marker_color', [0.8, 0.8, 0.1])
-        print(f'color: {color}')
+        self.color = self.get_param('marker_color', [0.8, 0.8, 0.1])
         object_file = self.get_param('object_file', '')
         with open(object_file, 'r') as f:
             configuration = yaml.safe_load(f)
-        print(f'configuration: {configuration}')
+        self.artifact_pub = self.create_publisher(MarkerArray, 'artifacts', 10)
+
+        self.publish_artifacts(configuration["objects"])
+
+        Logger.LogInfo(
+            'ObjectPublisher: Initializing done. Publishing objects...')
+
+    def publish_artifacts(self, artifacts):
+        markers = self.create_artifacts(artifacts)
+        self.artifact_pub.publish(markers)
+
+    def create_artifacts(self, artifacts):
+        if artifacts is None and not isinstance(artifacts, list):
+            return
+        ts_now = self.get_clock().now()
+        index = 0
+
+        marker_color = ColorRGBA()
+        marker_color.r = self.color[0]
+        marker_color.g = self.color[1]
+        marker_color.b = self.color[2]
+        marker_color.a = 1.0
+
+        artifact_markers = MarkerArray()
+        for art in artifacts:
+            cube = self.create_artifact(art, ts_now, index, marker_color)
+            artifact_markers.markers.append(cube)
+            index += 1
+        return artifact_markers
+
+    def create_artifact(self, artifact, ts_now, index, color):
+        (x, y, z) = artifact['position']
+
+        cube = Marker()
+        cube.header.frame_id = 'map'
+        cube.header.stamp = ts_now.to_msg()
+
+        cube.id = index
+        cube.action = Marker.ADD
+        cube.pose.position.x = x
+        cube.pose.position.y = y
+        cube.pose.position.z = z
+        cube.pose.orientation.w = 1.0
+
+        cube.color = color
+        cube.scale.x = cube.scale.y = cube.scale.z = 3.0
+
+        cube.type = Marker.CUBE
+        cube.frame_locked = True
+
+        return cube
 
     def get_param(self, key, default):
         self.declare_parameter(key, default)
